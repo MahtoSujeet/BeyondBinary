5bd8966480ecbedbb5e290425da269c54f370243
MahtoSujeet <sujeetmehta6@gmail.com>
2024-01-25 19:27:46 +0530

    Updates

---
+++ precache-manifest.eb32a37df262947425bfafac9ab6c526.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f18f91f3fd93a2774c23f97a1014eccc",
+    "url": "/BeyondBinary/index.html"
+  },
+  {
+    "revision": "4f9f7bb6e3952adb6149",
+    "url": "/BeyondBinary/static/css/2.f3497eb8.chunk.css"
+  },
+  {
+    "revision": "acf136f772792c6ae04c",
+    "url": "/BeyondBinary/static/css/main.fe2a8959.chunk.css"
+  },
+  {
+    "revision": "4f9f7bb6e3952adb6149",
+    "url": "/BeyondBinary/static/js/2.8e1db393.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/BeyondBinary/static/js/2.8e1db393.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "acf136f772792c6ae04c",
+    "url": "/BeyondBinary/static/js/main.f3eb83ea.chunk.js"
+  },
+  {
+    "revision": "12dd4c58795a818a40f6",
+    "url": "/BeyondBinary/static/js/runtime-main.d551f7e4.js"
+  },
+  {
+    "revision": "89542d397ebf623539dd073879c84ec1",
+    "url": "/BeyondBinary/static/media/binary.89542d39.jpg"
+  },
+  {
+    "revision": "684af6eaf4d5c58ad4463ea1a5e37163",
+    "url": "/BeyondBinary/static/media/crown.684af6ea.png"
+  },
+  {
+    "revision": "21c7355f4776d21ccb5b00bf1a1b1010",
+    "url": "/BeyondBinary/static/media/maskShapesNew.21c7355f.svg"
+  },
+  {
+    "revision": "7f3c1a9c75a67f913596e63579243b0c",
+    "url": "/BeyondBinary/static/media/nqueens.7f3c1a9c.jpg"
+  },
+  {
+    "revision": "51b8bef455414da4c9e1e70f962c8c61",
+    "url": "/BeyondBinary/static/media/path.51b8bef4.jpg"
+  },
+  {
+    "revision": "d0c447c28b4a285b0bd6de621c657854",
+    "url": "/BeyondBinary/static/media/sorting.d0c447c2.jpg"
+  },
+  {
+    "revision": "5985a426e3300a8293ca427b67be291e",
+    "url": "/BeyondBinary/static/media/word_search.5985a426.jpg"
+  }
+]);
